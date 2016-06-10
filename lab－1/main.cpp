f644604682099222aba9c6cc0572a8e208cff3a2
sunnyyouzi <fengmingyang@fengmingyangdemacbook-air.local>
2016-06-10 21:08:10 +0800

    Initial Commit

---
+++ lab－1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab－1
+//
+//  Created by 冯铭扬 on 16/6/10.
+//  Copyright © 2016年 冯铭扬. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
